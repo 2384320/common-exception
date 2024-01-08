19e6af65d122a84393e4f3316fd02969e5b693d7
2384320 <chdms1549@gmail.com>
2024-01-08 22:18:15 +0900

    chore: 초기 환경 구축

---
+++ src/test/java/com/commonexception/CommonExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
